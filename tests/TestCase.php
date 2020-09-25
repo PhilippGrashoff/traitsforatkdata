215b338c189f08b649bc5bcc2cd33f30524727fa
Philipp Reisigl <outdoor@spame.de>
2020-09-25 17:42:22 +0200

    add testcase with sqlite code

---
+++ tests/TestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace traitsforatkdata\tests;
+
+use atk4\data\Persistence;
+use atk4\schema\Migration;
+
+class TestCase extends \atk4\core\AtkPhpunit\TestCase {
+
+    protected $sqlitePersistenceModels = [];
+
+
+    protected function getSqliteTestPersistence(array $additionalClasses = []): Persistence {
+        $allClasses = array_merge($this->sqlitePersistenceModels, $additionalClasses);
+        $persistence = Persistence::connect('sqlite::memory:');
+        foreach($allClasses as $className) {
+            $model = new $className($persistence);
+            Migration::of($model)->drop()->create();
+        }
+
+        return $persistence;
+    }
+}
